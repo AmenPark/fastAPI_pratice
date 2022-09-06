1723350f8a79e850c8c03f3f35f152c864ff79d2
AmenPark <puzzlistpam@gmail.com>
2022-09-06 21:03:46 +0900

    add using return_model

--- main.py
+++ main.py
@@ -44,6 +44,31 @@ typing에서 List를 가져와서 하나의 타입으로만 값을 갖는 리스
 set도 비슷하게 가능하며, dict는 key-value 각각 타입 지정이 가능하다.
 
 비슷한 예시로 pydantic에서 HttpUrl타입을 가져올 수 있다. 이 검증은 대표적으로 이미지에 좋을 것이다.
+
+클래스 내부에 config 클래스를 정의하고, schema_extra를 이용해서 예시를 만들 수 있다.
+이 예시는 JSON 스키마로서 API 요청 docs에 표시된다.
+
+쿠키 사용은 fastapi.Cookie를 사용한다. 쿼리매개변수와 사용법은 동일한데 매개변수로 줄 때에 Cookie로 줘야만 한다.
+사용법은 def func1 (CK : Union[str, None] = Cookie(default = None)) 꼴로 사용.
+
+헤더 또한 마찬가지로 사용 가능하다. Header로 바뀐다는 점만 다르다. 거기에 추가 기능이 있다.
+헤더는 -(하이픈)문자를 통해서 구분하는데 파이썬에서 _와 자동 호환하는 기능이 존재한다. 파이썬 변수명은 a-b는 될 수 없어서.
+이 기능은 Header의 매개변수 convert_underscores가 디폴트 True값일때 작동하므로 False로 바꿔 끌 수 있다.
+여러 헤더라면 리스트 형태로 받는식으로 해결가능.
+
+response도 모델을 이용해서 가능. 이는 데코레이터 경로로 들어간다.
+@app.post("/items/", response_model = Item)
+이라고 하면 Item class가 response_model이 되는 것이다.
+모델에 맞지 않는 출력을 할 경우 적당히 없앨 attribute는 알아서 제거해주는데 빈건 어떻게 채울지는 모르겠다.
+None으로 채워주나? 그러기에는 type까지 제한 줘서 빡세게 할텐데..
+모델 수가 제한되어 있고, 모델의 일부분만 넘겨 줄 때는 response_model_exclude, response_model_include를 쓴다.
+사용법은 response_model과 마찬가지로 데코레이터 부분에 인자로 넘겨진다.
+기본적으로는 response_model_exclude = {"tax", "tags"}와 같이 set로 쓰이며, 리스트로 넘겨줘도 set으로 자동변환된다.
+각각 이것만, 이걸 제외하고? 정도로 쓰인다.
+
+데코레이터에 응답코드 매개변수를 넣을 수 있다. status_code = 201과 같은 방식.
+HTTP 상태코드를 받는다. 응답에서 해당 상태코드를 반환한다. 문서화에 추가한다.
+
 '''
 
 class Item(BaseModel):
@@ -52,6 +77,17 @@ class Item(BaseModel):
     price : float
     tax : Union[float, None] = None
     tags : List[str] = []
+
+    class config:
+        schema_extra = {
+            "example" : {
+                "name" : "golden sword",
+                "description" : "T1 sword",
+                "price" : 35.4,
+                "tax" : 3.5,
+                "tags" : ["T1", "oneHandWeapone", "light"],
+            }
+        }
 
 class Image(BaseModel):
     url : HttpUrl
